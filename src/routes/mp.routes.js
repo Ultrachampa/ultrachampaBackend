51f17310544713333da02bae899cdfc7cf08c4a4
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2023-07-30 18:05:30 -0300

    HOT FIX: REMOVED ADMIN REQUIREMENT ON MP.ROUTES

--- src/routes/mp.routes.js
+++ src/routes/mp.routes.js
@@ -4,10 +4,10 @@ import { payFee, receiveWebhook } from "../controllers/mp.controllers";
 
 const router = Router();
 
-router.post("/paymentFees", [verifyToken, isAdmin], payFee);
-router.get("/webhookMP/:feeID", [verifyToken, isAdmin], receiveWebhook);
-router.get("/success", [verifyToken, isAdmin], receiveWebhook);
-router.get("/failureUrl", [verifyToken, isAdmin], receiveWebhook);
-router.get("/pendingUrl", [verifyToken, isAdmin], receiveWebhook);
+router.post("/paymentFees", [verifyToken], payFee);
+router.get("/webhookMP/:feeID", [verifyToken], receiveWebhook);
+router.get("/success", [verifyToken], receiveWebhook);
+router.get("/failureUrl", [verifyToken], receiveWebhook);
+router.get("/pendingUrl", [verifyToken], receiveWebhook);
 
 export default router;
