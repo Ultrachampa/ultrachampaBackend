beba16011a606c293855e991a20b26f986d76db5
Nico Olivera <oliveran9@gmail.com>
2023-08-04 03:51:28 -0300

    Hermosa maniana verdad

--- src/routes/mp.routes.js
+++ src/routes/mp.routes.js
@@ -5,7 +5,7 @@ import { payFee, receiveWebhook } from "../controllers/mp.controllers";
 const router = Router();
 
 router.post("/paymentFees", [verifyToken], payFee);
-router.get("/webhookMP/:feeID", [verifyToken], receiveWebhook);
+router.post("/webhookMP/:feeID", receiveWebhook);
 router.get("/success", [verifyToken], receiveWebhook);
 router.get("/failureUrl", [verifyToken], receiveWebhook);
 router.get("/pendingUrl", [verifyToken], receiveWebhook);
