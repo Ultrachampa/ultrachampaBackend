e1c3ab1c127a53ad470812d45a4f4ff6cc7bb236
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2023-10-27 16:12:46 -0300

    test 3 response data

--- src/controllers/test.controllers.js
+++ src/controllers/test.controllers.js
@@ -21,7 +21,7 @@ const fetch = require("node-fetch");
 
 //   getDataExternService()
 
-export const test = (req, res) => {
+export const test = async (req, res) => {
   const { email, firstName, lastName, dob, Auth } = req.body;
   const datos = {
     email: email,
@@ -33,20 +33,17 @@ export const test = (req, res) => {
   // const url = `https://api.utmb.world/registration/checkActiveStatus?email=${datos.email}&lastName=${datos.lastName}firstName=${datos.firstName}&dob=${datos.dob}`;
   const url = "https://api.utmb.world/users/dashboard/member/simple";
 
-  fetch(url, {
+  const fetch = await fetch(url, {
     method: "GET",
     headers: {
       Authorization: Auth,
     },
   })
-    .then((response) => response.json())
+    .then((response) => response.status(200).json())
     .then((json) => console.log(json))
     .catch((err) => console.log(err));
 
-    
-
-
-
+    return fetch
 };
 
 // export const test = async (req, res) => {
